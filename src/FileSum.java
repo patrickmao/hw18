b7740acebdc3932a23b468b987493b05c38ef246
Patrick Mao <patrickmao@Patricks-MacBook-Pro.local>
2015-11-14 20:06:28 -0500

    initial commit

---
+++ src/FileSum.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by patrickmao on 11/13/15.
+ */
+public class FileSum {
+    public static double sumFile(String filename) {
+        if (filename == null) return 0;
+        double sum = 0;
+        Scanner s;
+        try {
+            FileReader fr = new FileReader(filename);
+            s = new Scanner(fr);
+        } catch (FileNotFoundException e) {
+            return 0;
+        }
+        while (s.hasNext()) {
+            if (s.hasNextInt() || s.hasNextDouble() || s.hasNextFloat()) {
+                sum += Double.parseDouble(s.next());
+            } else {
+                s.next();
+            }
+        }
+        return sum;
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by patrickmao on 11/13/15.
+ */
+public class test {
+    public static void main(String[] args) {
+        FileSum fs = new FileSum();
+
+    }
+}
